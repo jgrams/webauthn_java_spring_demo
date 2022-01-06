893c751a6987d1c147ef21373186ae34e6ed9f2c
jgrams <tynanjk@gmail.com>
2022-01-06 17:23:12 -0600

    added webauth server, added rp initialization

--- src/main/java/com/webauthn/app/AppApplication.java
+++ src/main/java/com/webauthn/app/AppApplication.java
@@ -1,13 +1,31 @@
 package com.webauthn.app;
 
+import com.webauthn.app.data.repository.RegistrationRepository;
+import com.yubico.webauthn.RelyingParty;
+import com.yubico.webauthn.data.RelyingPartyIdentity;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
+	private final RelyingParty rp;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+		configureWebAuthServer();
+	}
+
+	public static void configureWebAuthServer() {
+		RelyingPartyIdentity rpIdentity = RelyingPartyIdentity.builder()
+			.id("example.com")
+			.name("Yubi Application")
+			.build();
+		RelyingParty rp = RelyingParty.builder()
+			.identity(rpIdentity)
+			.credentialRepository(new RegistrationRepository())
+			.validateSignatureCounter(true)
+			.build();
 	}
 
 }
